e26729dfa77dd8df305deac85a357efd75b41407
Barbara <Barbara@192.168.1.44>
2019-09-15 13:55:17 -0300

    visibilidade alterada e setters incluídos Classe PapelMoedaInvalidaException implementada

---
+++ src/PapelMoedaInvalidaException.java
@@ -0,0 +1,16 @@
+
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+public class PapelMoedaInvalidaException extends Exception {
+   
+    // CÒDIGO INCOMPLETO. [COMISSÃO]
+
+    public PapelMoedaInvalidaException() {
+        super("Nota Inválida!\nRetire a nota para continuar a operação.");
+    }
+}
+
+
