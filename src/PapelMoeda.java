e602f3173b56a8709f9d980456925e16b047d8c8
Barbara <Barbara@192.168.1.44>
2019-09-15 13:54:07 -0300

    visibilidade alterada e setters incluídos

---
+++ src/PapelMoeda.java
@@ -0,0 +1,41 @@
+
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+/**
+ * SETERS faltando, e não representados na documentação.
+ * [COMISSÃO]
+ */
+public class PapelMoeda {
+
+    /**
+     * Visibilidade dos atributos não bate com o especificado
+     * 
+     * [COMISSÃO]
+     */
+    private int valor; //visibilidade alterada de protected para private
+    private int quantidade; //visibilidade alterada de protected para private
+ 
+    public PapelMoeda(int valor, int quantidade) {
+        this.valor = valor;
+        this.quantidade = quantidade;
+    }  
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+    
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
