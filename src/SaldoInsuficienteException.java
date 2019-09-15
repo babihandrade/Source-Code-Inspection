0fd68a1b90ea17a82785021847528772d3be0c21
Barbara <Barbara@192.168.1.44>
2019-09-15 13:56:13 -0300

    visibilidade alterada e setters incluídos Classe PapelMoedaInvalidaException implementada Classe SaldoInsuficienteException implementada

---
+++ src/SaldoInsuficienteException.java
@@ -0,0 +1,13 @@
+
+
+/**
+ *
+ * @author Calebe de Paula Bianchini
+ */
+public class SaldoInsuficienteException extends Exception {
+   
+    // CÒDIGO INCOMPLETO. [COMISSÃO]
+        public SaldoInsuficienteException() {
+        super("Saldo Insuficiente!");
+    }
+}
